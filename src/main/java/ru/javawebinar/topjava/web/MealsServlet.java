b80e473184e06cd051264a2f981c007342a1b359
Andrey Butko <andrey.butko@gmail.com>
2017-03-26 17:28:59 +0500

    1 6 logging

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -6,22 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-
 /**
- * User: gkislin
- * Date: 19.08.2014
+ * Created by butkoav on 25.03.2017.
  */
-public class UserServlet extends HttpServlet {
-    private static final Logger LOG = getLogger(UserServlet.class);
-
+public class MealsServlet extends HttpServlet {
+private static final Logger LOG = getLogger(MealsServlet.class);
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to users");
+        LOG.debug("redirect to meals");
 
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+        response.sendRedirect("meals.jsp");
     }
 }
