66408b320c1c67799f4187525d9527c463a441f3
Andrey Butko <andrey.butko@gmail.com>
2017-03-27 14:27:44 +0500

    lastID from Long to AtomicLong

--- src/main/java/ru/javawebinar/topjava/storage/MealsStorageInMemory.java
+++ src/main/java/ru/javawebinar/topjava/storage/MealsStorageInMemory.java
@@ -8,21 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by butkoav on 26.03.2017.
  */
 public class MealsStorageInMemory implements MealsStorage {
     private final Map<Long, Meal> meals = new ConcurrentHashMap<>();
-    private Long lastId = 0l;
+    private AtomicLong lastId = new AtomicLong(0l);
 
     public MealsStorageInMemory() {
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
-        meals.put(++lastId, new Meal(lastId, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        meals.put(lastId.incrementAndGet(), new Meal(lastId.get(), LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
     }
 
     @Override
@@ -32,11 +33,8 @@ public class MealsStorageInMemory implements MealsStorage {
 
     @Override
     public void add(LocalDateTime dateTime, String description, int calories) {
-        long id;
-        synchronized (lastId) {
-            id = ++this.lastId;
-        }
-        meals.put(id, new Meal(id, dateTime, description, calories));
+
+        edit(0l, dateTime, description, calories);
     }
 
     @Override
@@ -46,6 +44,8 @@ public class MealsStorageInMemory implements MealsStorage {
 
     @Override
     public void edit(long id, LocalDateTime dateTime, String description, int calories) {
+        if (id == 0l)
+            id = lastId.incrementAndGet();
         if (meals.containsKey(id))
             meals.put(id, new Meal(id, dateTime, description, calories));
     }
