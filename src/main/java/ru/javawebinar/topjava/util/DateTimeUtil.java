2d87d1713394878cea22916f8aa5e10a3a8be1c6
Andrey Butko <andrey.butko@gmail.com>
2017-04-02 23:56:26 +0500

    HW02 Commit

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -12,13 +12,18 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+  /*  public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
+
     public static boolean isBetween(LocalDate lt, LocalDate startDate, LocalDate endDate) {
         return lt.compareTo(startDate) >= 0 && lt.compareTo(endDate) <= 0;
+    }*/
+
+    public static <T extends Comparable> boolean isBetween(T current, T start, T end) {
+        return current.compareTo(start) >= 0 && current.compareTo(end) <= 0;
     }
-  /*  public static boolean isBetweenD(<? extends T>)*/
+
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
