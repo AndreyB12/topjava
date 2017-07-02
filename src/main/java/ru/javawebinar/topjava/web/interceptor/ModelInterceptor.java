ce0b35fdd57ea28f8b4cf39e7d80dec846d59857
Andrey Butko <andrey.butko@gmail.com>
2017-07-02 18:51:24 +0500

    HW 10

--- src/main/java/ru/javawebinar/topjava/web/interceptor/ModelInterceptor.java
+++ src/main/java/ru/javawebinar/topjava/web/interceptor/ModelInterceptor.java
@@ -2,12 +2,10 @@ package ru.javawebinar.topjava.web.interceptor;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.support.RequestContextUtils;
 import ru.javawebinar.topjava.AuthorizedUser;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
 
 /**
  * This interceptor adds the user to the model of every requests managed
@@ -20,8 +18,6 @@ public class ModelInterceptor extends HandlerInterceptorAdapter {
             AuthorizedUser authorizedUser = AuthorizedUser.safeGet();
             if (authorizedUser != null) {
                 modelAndView.getModelMap().addAttribute("userTo", authorizedUser.getUserTo());
-            } else if (request.getParameterMap().containsKey("language")) {
-                RequestContextUtils.getLocaleResolver(request).setLocale(request, response, new Locale(request.getParameter("lang")));
             }
         }
     }
